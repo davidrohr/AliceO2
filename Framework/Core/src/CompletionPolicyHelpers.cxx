fe18f8da8aeef7f1bfb9a3db2087213615b0d953
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-04-03 19:05:45 +0200

    Use newly introduced ConsumeAndRescan

--- Framework/Core/src/CompletionPolicyHelpers.cxx
+++ Framework/Core/src/CompletionPolicyHelpers.cxx
@@ -123,7 +123,7 @@ CompletionPolicy CompletionPolicyHelpers::consumeWhenAllOrdered(const char* name
       }
     }
     (*nextTfCounter)++;
-    return CompletionPolicy::CompletionOp::Consume;
+    return CompletionPolicy::CompletionOp::ConsumeAndRescan;
   };
   return CompletionPolicy{name, matcher, callback};
 }
